f50efdb0eadbdbd6691559fd78a8536495b2cdd7
Lupo Montero <lupomontero@gmail.com>
2020-12-02 19:11:51 -0500

    Removes obsoleted commented out code

--- exercises/05-mayores-menores/solutions/analyseArray.js
+++ exercises/05-mayores-menores/solutions/analyseArray.js
@@ -1,18 +1,3 @@
-// const analyseArray = (arr) => {
-//   const result = [arr[0], arr[0], arr[0]];
-
-//   for (let i = 1; i < arr.length; i += 1) {
-//     if (arr[i] > result[0]) {
-//       result[2] = result[0];
-//       result[0] = arr[i];
-//     } else if (arr[i] < result[1]) {
-//       result[1] = arr[i];
-//     }
-//   }
-
-//   return result;
-// };
-
 const analyseArray = (arr) => {
   const result = [arr[0], arr[0], undefined];
 

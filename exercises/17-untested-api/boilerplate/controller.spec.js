39f500ebd3f2725986542c2cd5332a9a63007eb2
Fares A. Akel C <f.antonio.akel@gmail.com>
2021-10-04 07:05:22 -0500

    Adds backend (express) exercise

---
+++ exercises/17-untested-api/boilerplate/controller.spec.js
@@ -0,0 +1,17 @@
+const controller = require('./controller')
+
+jest.mock('lorem-ipsum');
+
+describe('/lorem controller', () => {
+  it('should require a length', () => {
+  })
+
+  it('should require a positive integer', () => {
+  })
+
+  it('should require a number', () => {
+  })
+
+  it('should respond with a single word', () => {
+  })
+})
